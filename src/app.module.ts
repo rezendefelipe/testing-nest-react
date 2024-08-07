38a6f63b3ee52d0f2c524a7ea8371de345986757
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-07 11:54:56 -0300

    feat: add conection do db and orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      // port: Number(process.env.DB_PORT),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
