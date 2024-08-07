2c67c94bc9c9e4b0a48dacc2acccf9dbe9a26ce7
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-07 11:10:24 -0300

    feat: add post create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
   @Get()
   async getAllUsers() {
-    return JSON.stringify({ test: 'abcc' });
+    return JSON.stringify({ user: 'some user details' });
+  }
+
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return createUser;
   }
 }
