cc924286dbfcc1ec73f68620cce7e44f4cd9c48e
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-07 17:19:52 -0300

    feat: add get state ith service and entity

---
+++ src/address/entities/address.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'address' })
+export class AddressEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'user_id', nullable: false })
+  userId: number;
+  @Column({ name: 'complement', nullable: true })
+  complement: string;
+  @Column({ name: 'number', nullable: false })
+  number: string;
+  @Column({ name: 'cep', nullable: false })
+  cep: string;
+  @Column({ name: 'city_id', nullable: false })
+  cityId: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'update_at' })
+  updateAt: Date;
+}
