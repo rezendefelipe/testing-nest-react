4eebef37aac5565d842772a98d55516cfe15af8f
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-08 15:08:56 -0300

    feat: add pipe to filter data on flow and return dto to api

---
+++ src/user/dtos/returnUser.dto.ts
@@ -0,0 +1,17 @@
+import { UserEntity } from '../entities/user.entity';
+
+export class ReturnUserDto {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  doc: string;
+
+  constructor(userEntity: UserEntity) {
+    this.id = userEntity.id;
+    this.name = userEntity.name;
+    this.email = userEntity.email;
+    this.phone = userEntity.phone;
+    this.doc = userEntity.doc;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getAllUsers() {
-    return this.userService.getAllUsers();
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUsers()).map(
+      (user) => new ReturnUserDto(user),
+    );
   }
 
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
     return this.userService.createUserService(createUser);
