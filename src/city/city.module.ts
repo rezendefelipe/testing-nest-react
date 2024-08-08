e0151e98c049fa5f2dc4c7c591814d018ca68fa6
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-08 11:36:15 -0300

    feat: create city controller and cache list cities

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([CityEntity]),
+    CacheModule.register({
+      ttl: 5,
+    }),
+  ],
+  controllers: [CityController],
+  providers: [CityService],
+})
 export class CityModule {}
