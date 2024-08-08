96b6c01d16a30c62b59b749e970daaa26f80a361
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-08 19:17:28 -0300

    feat: add dtos to return address with the user obj

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -49,7 +49,13 @@ export class UserService {
       where: {
         id: userId,
       },
-      relations: ['addresses'],
+      relations: {
+        addresses: {
+          city: {
+            state: true,
+          },
+        },
+      },
     });
 
     if (!user) {
