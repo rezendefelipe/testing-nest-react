f24f0c39c84aff357eb6c4e85cd77bb83e102d60
Felipe Rezende <felipevrezende@hotmail.com>
2024-08-08 18:59:28 -0300

    feat: add relations state cities and address

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,3 +1,4 @@
+import { CityEntity } from 'src/city/entities/city.entity';
 import { UserEntity } from 'src/user/entities/user.entity';
 import {
   Column,
@@ -34,4 +35,8 @@ export class AddressEntity {
   @ManyToOne(() => UserEntity, (user) => user.addresses)
   @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
   userEntity: UserEntity;
+
+  @ManyToOne(() => CityEntity, (city) => city.addresses)
+  @JoinColumn({ name: 'city_id', referencedColumnName: 'id' })
+  city: CityEntity;
 }
--- src/state/entities/state.entity.ts
+++ src/state/entities/state.entity.ts
@@ -1,7 +1,9 @@
+import { CityEntity } from 'src/city/entities/city.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -21,4 +23,7 @@ export class StateEntity {
 
   @UpdateDateColumn({ name: 'update_at' })
   updateAt: Date;
+
+  @OneToMany(() => CityEntity, (city) => city.state)
+  cities?: CityEntity[];
 }
